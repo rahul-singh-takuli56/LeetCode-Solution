0dd3399b3abf5613aad1ee23ee17bb003012c35f
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-02-05 19:24:25 +0530

    Time: 7 ms (76.94%), Space: 7.1 MB (98.95%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-        bool check(vector<int>a,vector<int>b){
-            for(int i=0;i<26;i++){
-                if(a[i]!=b[i])
-                    return false;
-            }
-            return true;
-        }
-public:
     bool checkInclusion(string s1, string s2) {
         if(s2.size()<s1.size())
             return false;
-        vector<int>frq1(26,0),frq2(26,0);
-        for(auto it:s1)
-            frq1[it-'a']++;
-        int i=0,j=0,k=s1.size();
-        while(j<s2.size()){
-            frq2[s2[j]-'a']++;
-            if(j-i+1==k){
-                if(check(frq1,frq2))
-                    return true;
-            }
-            
-            if(j-i+1<k)
-                j++;
-           
-            else{
-                frq2[s2[i]-'a']--;
-                i++;j++;
-            }
+        int k=s1.size();
+        int i=0,j=0;
+        vector<int>fs1(26,0);
+        vector<int>fs2(26,0);
+        for(int i=0;i<k;i++){
+            fs1[s1[i]-'a']++;
+            fs2[s2[i]-'a']++;
+        }
+        if(fs1==fs2)
+            return true;
+        for(int i=k;i<s2.size();i++){
+            fs2[s2[i-k]-'a']--;
+            fs2[s2[i]-'a']++;
+            if(fs1==fs2)
+                return true;
         }
         return false;
     }
