58ab26d048a0590a9e80f2d8293b6d5861d165a6
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-02-05 19:05:16 +0530

    Time: 11 ms (91.91%), Space: 8.7 MB (77.40%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,46 +1,23 @@
 class Solution {
-	public:
-		vector<int> findAnagrams(string s, string p) {
-			unordered_map<char,int>mp;
-			int k = p.size();
-			for(int i=0; i<k; i++) {
-				mp[p[i]]++;           
-			}
-			int count = mp.size();     
-        
-        vector<int> ans;
-        int i=0, j=0;
-        
-        while(j<s.size()) {
-            
-            if(mp.find(s[j]) != mp.end()) {  
-                mp[s[j]]--;                  
-                
-                if(mp[s[j]]==0) {            
-                    count--;
-                }  
-            }
-            if((j-i+1) < k) {
-                j++;
-            }
-            
-            else if((j-i+1) == k) {
-                
-                if(count==0) {
-                    ans.push_back(i);
-                }
-                
-                if(mp.find(s[i])!=mp.end()) {
-                    mp[s[i]]++;
-                    
-                    if(mp[s[i]]==1) {
-                        count++;
-                    }
-                }
-                i++;
-                j++;
-                
-            }
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int>ans;
+        vector<int>fs(26,0);
+        vector<int>fp(26,0);
+        int k=p.size();
+        if(s.size()<p.size())
+            return ans;
+        for(int i=0;i<k;i++){
+            fs[s[i]-'a']++;
+            fp[p[i]-'a']++;
+        }
+        if(fs==fp)
+           ans.push_back(0);
+        for(int i=k;i<s.size();i++){
+            fs[s[i-k]-'a']--;
+            fs[s[i]-'a']++;
+            if(fs==fp)
+                ans.push_back(i-k+1);
         }
         return ans;
     }
