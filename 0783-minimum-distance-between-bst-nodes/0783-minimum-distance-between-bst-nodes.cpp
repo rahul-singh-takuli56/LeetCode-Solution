99eaf672d4d21c9fbeb7fda39236c6bf33ee74dd
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-02-17 23:53:20 +0530

    Time: 4 ms (56.01%), Space: 9.7 MB (69.86%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -1,19 +1,20 @@
 class Solution {
-    void inorder(TreeNode*root,vector<int>&ans){
-        if(root==NULL)
-            return ;
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-    }
-public:
      // if you are preorder or postorder then you will have to sort 
     // the ans vector then only mini will get correct answere
     // or we can use inorder traversal it always give answer in sorted         // manner.
+    void preorder(TreeNode*root,vector<int>&ans){
+        if(root==NULL)
+            return ;
+        ans.push_back(root->val);
+        preorder(root->left,ans);
+        preorder(root->right,ans);
+    }
+public:
     int minDiffInBST(TreeNode* root) {
         vector<int>ans;
         int mini=INT_MAX;
-        inorder(root,ans);
+        preorder(root,ans);
+        sort(ans.begin(),ans.end());
         for(int i=1;i<ans.size();i++){
             int diff=abs(ans[i]-ans[i-1]);
             mini=min(mini,diff);
