3e565d8fc20f1d6d302a207fb1944e2e7bee85c6
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-02-21 22:10:23 +0530

    Time: 34 ms (32.30%), Space: 22.3 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
+    //op method
     int singleNonDuplicate(vector<int>& nums) {
-         int i=1;
-        while(i<nums.size()){
-            if(nums[i-1]!=nums[i])
-                return nums[i-1];
-               i=i+2;
+        int low=0,high=nums.size()-2; // -2 beacause last element
+        // agr single hogha to hmara low apne aap point kregha
+        // use and in the end we're returning nums[low];
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==nums[mid^1])
+                //logic of xor use hogha
+                //XOR IMP PROPERTY
+                //ODD^1=ODD-1;
+                //EVEN^1=EVEN+1;
+                low=mid+1;
+            else
+                high=mid-1;
         }
-        return nums[nums.size()-1];
+        return nums[low];
     }
 };
