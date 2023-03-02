5fc4eb47496c8dd64959c943cc2bb37afb10af7e
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-03-02 19:31:03 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.29%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int numJewelsInStones(string jewels, string stones) {
-        int count=0;
-        //O(n^2)
-        for(int i=0;i<jewels.size();i++){
-            for(int j=0;j<stones.size();j++){
-                if(jewels[i]==stones[j])
-                    count++;
-            }
+    //optimized solution
+    int numJewelsInStones(string J, string S) {
+    int count = 0;
+    for (char s : S) {
+        if (J.find(s) != string::npos) {
+            ++count;
         }
-        return count;
+    }
+    return count;
     }
 };
