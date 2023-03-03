44d888095b22b2a8e01b5fe421abe880fe79df0b
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-03-03 17:33:59 +0530

    Time: 7 ms (81.58%), Space: 15.1 MB (94.93%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        vector<pair<char, int>> arr;
-        string str="";
+        // better code than previous
+        string copy=s;
         for(int i=0;i<s.size();i++){
-            arr.push_back({s[i],indices[i]});
+            copy[indices[i]]=s[i];
         }
-        sort(arr.begin(), arr.end(), [](auto& p1, auto& p2) {
-            return p1.second < p2.second;
-        });
-        for(int i=0;i<arr.size();i++){
-            str += arr[i].first;
-        }
-        return str;
+        return copy;
     }
 };
