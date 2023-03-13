568c25864e67b81c3695076721327f6a2985983a
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-03-13 11:47:44 +0530

    Time: 7 ms (53.98%), Space: 16.3 MB (84.12%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,27 +11,19 @@
  */
 class Solution {
 public:
+    //Recursive approace
     bool isSymmetric(TreeNode* root) {
-        //Iterative approach
-        if(!root)
+        if(root==NULL)
             return true;
-        stack<TreeNode*>st;
-        st.push(root->left);
-        st.push(root->right);
-        while(!st.empty()){
-            auto p=st.top();
-            st.pop();
-            auto q=st.top();
-            st.pop();
-            if(!p and !q) //i.e if null value is prsent skip current iteration
-                continue;
-            if(!p or !q || p->val!=q->val)
-                return false;
-            st.push(p->left);
-            st.push(q->right);
-            st.push(p->right);
-            st.push(q->left);
-        }
-        return true;
+        return solve(root->left,root->right);
+    }
+    bool solve(TreeNode*left,TreeNode*right){
+        if(left==NULL and right==NULL)
+            return true;
+        else if(left==NULL or right==NULL)
+            return false;
+        else if(left->val != right->val)
+            return false;
+        return solve(left->left, right->right) and solve(left->right, right->left);
     }
 };
