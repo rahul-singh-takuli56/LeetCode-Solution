e419ce41af651e7853819fb7d2a7ed19df566681
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-03-17 14:12:22 +0530

    Time: 124 ms (19.90%), Space: 62 MB (33.66%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int deepestLeavesSum(TreeNode* root) {
-        int sum=0;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            sum=0;
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                auto top=q.front();
-                q.pop();
-                sum+=top->val;
-                if(top->left)
-                    q.push(top->left);
-                if(top->right)
-                    q.push(top->right);
-            }
-        }
+    int sum=0;
+    int sumEvenGrandparent(TreeNode* root) {
+        solve(root,NULL,NULL);
         return sum;
+    }
+    void solve(TreeNode*root,TreeNode*parent,TreeNode*grandparent){
+        if(!root)
+            return ;
+        if(grandparent!=NULL and grandparent->val%2==0){
+            sum+=root->val;
+        }
+        solve(root->left,root,parent);
+        solve(root->right,root,parent);
     }
 };
