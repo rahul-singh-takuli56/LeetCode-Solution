1e1bfe6d62bcf3b0f1dfee55fabde78d89254401
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-04-30 15:24:21 +0530

    Time: 7 ms (95.54%), Space: 9 MB (95.76%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
-    int subset(vector<int>&arr,int sum,int n){
-         int mod=1e9+7;
-	    int dp[n+1][sum+1];
+    int subset(vector<int>&nums,int sum,int n){
+        int mod=1e9+7;
+        int dp[n+1][sum+1];
         for(int i=0;i<n+1;i++){
             for(int j=0;j<sum+1;j++){
-                if(i==0) dp[i][j]=0;
-                if(j==0) dp[i][j]=1;
-            }
+                if(i==0)
+                    dp[i][j]=0;
+                if(j==0)
+                    dp[i][j]=1;
+            }   
         }
         for(int i=1;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-                if(arr[i-1]<=j)
-                    dp[i][j]=(dp[i-1][j])%mod + (dp[i-1][j-arr[i-1]])%mod;
+            for(int j=0;j<sum+1;j++){//remeber 0 here
+                if(nums[i-1]<=j)
+                    dp[i][j]=(dp[i-1][j])%mod + (dp[i-1][j-nums[i-1]])%mod;
                 else
                     dp[i][j]=(dp[i-1][j])%mod;
             }
         }
-        return (dp[n][sum])%mod;
-            
+        return dp[n][sum]%mod;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -26,8 +27,9 @@ public:
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
         }
-        if(sum < abs(target) || (target+sum) %2 != 0) return 0;
-        int s1=(sum+target)/2;
-        return subset(nums,s1,n);
+        if(abs(target)>sum || (sum+target)%2!=0)
+            return 0;
+        int sum1=(sum+target)/2;
+        return subset(nums,sum1,n);
     }
 };
