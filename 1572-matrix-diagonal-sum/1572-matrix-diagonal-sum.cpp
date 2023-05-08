9a9d110e161c446935f7726333238604d6b1dcfa
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-05-08 07:08:32 +0530

    Time: 22 ms (5.66%), Space: 11.3 MB (64.67%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -3,16 +3,12 @@ public:
     int diagonalSum(vector<vector<int>>& mat) {
         int sum=0;
         int n=mat.size();
-        int m=mat[0].size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==j || i+j==n-1)
-                    sum+=mat[i][j];
-                // if(i+j==n)
-                    // sum+=mat[i][j];
-            }
+            sum+=mat[i][i];
+            sum+=mat[n-1-i][i];
         }
-        cout<<n;
+        if(n%2!=0)
+            sum-=mat[n/2][n/2];
         return sum;
     }
 };
