1547bd3f655aaba558baa9d0d7a75fde33c3485a
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-05-16 21:27:35 +0530

    Time: 831 ms (5.03%), Space: 219.5 MB (5.07%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -1,26 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-    ListNode *n1 = nullptr, *n2 = nullptr;
-    for (auto p = head; p != nullptr; p = p->next) {
-        n2 = n2 == nullptr ? nullptr : n2->next;
-        if (--k == 0) {
-            n1 = p;
-            n2 = head;
+        vector<int> a;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            a.push_back(temp->val);
+            temp=temp->next;
+
         }
+        ListNode *h=NULL, *t=NULL;
+        swap(a[k-1],a[a.size()-k]);
+        ListNode* ans=new ListNode();
+        for(int i=0;i<a.size();i++){
+            ans->val=a[i];
+            if(h==NULL){
+                h=ans;
+                t=ans;
+            }
+            else{
+                t->next=ans;
+                t=ans;
+            }
+            ans=new ListNode();
+        }
+        return h;
     }
-	// The problem description specifically asks to swap values, not nodes themselves. 
-    swap(n1->val, n2->val);
-    return head;
-}
 };
