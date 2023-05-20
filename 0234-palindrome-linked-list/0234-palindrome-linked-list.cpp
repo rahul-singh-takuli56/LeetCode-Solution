dd31c2df07809ab8dfe714dfca16b274f5d28f7e
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-05-20 20:02:16 +0530

    Time: 270 ms (30.40%), Space: 117.9 MB (62.59%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,26 +1,46 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int>ans;
-        while(head!=nullptr){
-            ans.push_back(head->val);
-            head=head->next;
+    ListNode* reverse_list(ListNode* head){
+        ListNode* pre=NULL;
+        ListNode* next=NULL;
+        while(head!=NULL){
+            next=head->next;
+            head->next=pre;
+            pre=head;
+            head=next;
         }
-        int i=0,j=ans.size()-1;
-        while(i<j){
-            if(ans[i]!=ans[j])
+        return pre;
+    }
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head==nullptr || head->next==nullptr)
+            return true;
+        
+        // just two pointer slow wil move by 1 step
+        // fast will move with two step
+        ListNode* slow=head;
+        ListNode* fast=head;
+        
+        
+        //finding the left mid element
+        //when fast will reach to last or second last node then at that moment we will get mid element 
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        
+        
+        //reversing second half of linked list 
+        slow->next=reverse_list(slow->next);
+        slow=slow->next;\
+            
+        // to check palindrome hai ke nhee 
+        // by comparing first elment of first half and first element of second half and so on ....
+        while(slow!=NULL){
+            if(head->val!=slow->val)
                 return false;
-            i++;j--;
+            slow=slow->next;
+            head=head->next;
         }
         return true;
     }
