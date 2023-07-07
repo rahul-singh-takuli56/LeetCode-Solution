4b4de6b5ad9ba8cd82886aad7ebdc83a540bcbce
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-07-07 23:29:41 +0530

    Time: 118 ms (79.04%), Space: 67.8 MB (37.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,8 +5,9 @@ public:
         int curr=0;
         for(int i=0;i<nums.size();i++){
             curr+=nums[i];
-            if(curr>maxi)
-                maxi=curr;
+            maxi=max(maxi,curr);
+            // if(curr>maxi)
+                // maxi=curr;
             if(curr<0)
                 curr=0;
         }
