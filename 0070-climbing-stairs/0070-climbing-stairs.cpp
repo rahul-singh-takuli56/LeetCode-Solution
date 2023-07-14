94f8e1caf7e52c94c48e97bcbbe7ca5cb64cba5b
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-07-15 01:07:32 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (13.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
+        vector<int>dp(n,-1);
        return solve(n,dp);
     }
     int solve(int n,vector<int>&dp){
         if(n==1 || n==2)
             return n;
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        if(dp[n-1]!=-1)
+            return dp[n-1];
+        return dp[n-1]=solve(n-1,dp)+solve(n-2,dp);
     }
 };
