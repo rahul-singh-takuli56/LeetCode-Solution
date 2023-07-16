f5b2e997d89d3848723d46fb2791670964624235
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-07-16 19:05:04 +0530

    Time: 8 ms (57.33%), Space: 9 MB (20.01%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    //using recursive approach
-    int solve(int i,int j,vector<vector<int>>&nums,vector<vector<int>>&dp){
-        int n=nums.size();
-        if(i==n-1) // if i is in last row then return its value 
-            return nums[n-1][j];
-        
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        int down= nums[i][j]+solve(i+1,j,nums,dp);
-        int diag=nums[i][j]+solve(i+1,j+1,nums,dp);
-        return dp[i][j]= min(down,diag);
-    }
+    //using bottom up  approach
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return solve(0,0,triangle,dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        
+        //base case
+        for(int i=0;i<n;i++){
+            dp[n-1][i]=triangle[n-1][i];
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=dp[i+1][j]+triangle[i][j];
+                int dig=dp[i+1][j+1]+triangle[i][j];
+                dp[i][j]=min(down,dig);
+            }
+        }
+        return  dp[0][0];
     }
 };
