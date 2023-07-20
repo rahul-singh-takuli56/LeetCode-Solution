fdddd51e73aa5a16a1b16258e29b52e8d4c92116
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-07-21 03:44:34 +0530

    Time: 238 ms (30.72%), Space: 72.9 MB (55.26%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-        string s2=s;
-        reverse(s2.begin(),s2.end()); 
+        string str=s;
+        reverse(str.begin(),str.end());
+
         int n=s.size();
-        int m=s2.size();
-        int dp[n+1][m+1];
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<m+1;j++){
-                if(i==0 || j==0)
-                    dp[i][j]=0;
-            }
-        }
-        
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(s[i-1]==s2[j-1])
+        int m=str.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++)
+            dp[i][0]=0;
+        for(int j=0;j<=m;j++)
+            dp[0][j]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==str[j-1])
                     dp[i][j]=1+dp[i-1][j-1];
                 else
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
