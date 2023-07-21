f6349bc92e781a16bcd280ef23970f6520e90fdb
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-07-21 15:32:07 +0530

    Time: 79 ms (58.84%), Space: 28.5 MB (43.27%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    int lcs(string& s1, string& s2, int m, int n, vector<vector<int>>& memo) {
-        if (m == 0 || n == 0) {
-            return 0;
-        }
-        if (memo[m][n] != -1) {
-            return memo[m][n];
-        }
-        if (s1[m - 1] == s2[n - 1]) {
-            return memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
-        }
-        return memo[m][n] = max(lcs(s1, s2, m - 1, n, memo), lcs(s1, s2, m, n - 1, memo));
-    }
-
     int minInsertions(string s) {
-        int n = s.length();
-        string sReverse = s;
-        reverse(sReverse.begin(), sReverse.end());
-        vector<vector<int>> memo(n + 1, vector<int>(n + 1));
+        string str=s;
+        reverse(str.begin(),str.end());
+        int n=s.size();
+        int m=str.size();
+        
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++)
+            dp[i][0]=0;
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n; j++) {
-                memo[i][j] = -1;
+        for(int j=0;j<=m;j++)
+            dp[0][j]=0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==str[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
-
-        return n - lcs(s, sReverse, n, n, memo);
+        return abs(dp[n][m]-n);
     }
 };
