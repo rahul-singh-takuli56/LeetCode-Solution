ba5e9bdd97d0c0413b31344b0b987974b422346c
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-07-28 03:36:04 +0530

    Time: 8 ms (93.85%), Space: 8.3 MB (70.11%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,28 +1,28 @@
-bool sortBySecond(const pair<char, int>& a, const pair<char, int>& b) {
-    return a.second > b.second;
-}
 
+//this is good but if know how to use lambda function that will be better so that
+// we can use this function inside the same class
+bool sortbyfrq(pair<char,int>&a, pair<char,int>&b){
+        return a.second>b.second;
+}
 class Solution {
-public:
+public:   
     string frequencySort(string s) {
-        unordered_map<char, int> mp;
-        for (auto it : s)
+        unordered_map<char,int>mp;
+        for(auto it:s)
             mp[it]++;
         
-        vector<pair<char, int>> sortedPairs;
-        for (auto it : mp)
-            sortedPairs.push_back(it);
+        vector<pair<char,int>>sorted;
+        for(auto it:mp)
+            sorted.push_back(it);
+        sort(sorted.begin(),sorted.end(),sortbyfrq);
         
-        sort(sortedPairs.begin(), sortedPairs.end(), sortBySecond);
-        
-        string ans = "";
-        for (auto it : sortedPairs) {
-            while (it.second > 0) {
-                ans += it.first;
+        string ans="";
+        for(auto it:sorted){
+            while(it.second>0){
+                ans+=it.first;
                 it.second--;
             }
         }
-        
         return ans;
     }
 };
