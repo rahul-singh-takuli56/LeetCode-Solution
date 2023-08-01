1276860ab57667fb9d2dbba34b33cb2137a28079
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-08-01 06:52:55 +0530

    Time: 303 ms (79.08%), Space: 23.5 MB (36.11%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -4,16 +4,20 @@ public:
         int sum=0;
         for(int i=0;i<s.size();i++){
             unordered_map<char,int>mp;
-            
             for(int j=i;j<s.size();j++){
                 mp[s[j]]++;
+                int maxi=INT_MIN;
                 int mini=INT_MAX;
-                int maxi=INT_MIN;    
                 for(auto it:mp){
                     maxi=max(maxi,it.second);
                     mini=min(mini,it.second);
                 }
-                sum+=maxi-mini;
+                    sum+=maxi-mini;
+                // if(mp.size()>=2){
+                //     maxi=max(maxi,mp[s[j]]);
+                //     mini=min(mini,mp[s[j]]);
+                //     sum+=(maxi-mini);
+                // }
             }
         }
         return sum;
