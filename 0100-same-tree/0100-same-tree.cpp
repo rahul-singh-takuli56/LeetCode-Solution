754153b634b36ae00e317ebf0ab86ceb83bc4c6a
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-08-06 11:36:45 +0530

    Time: 4 ms (26.70%), Space: 10.1 MB (23.09%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,8 +1,31 @@
 class Solution {
 public:
+    //Iterative approach
+    //level order traversal
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL || q==NULL)
-            return p==q;
-        return (p->val==q->val) && isSameTree(p->left,q->left)  && isSameTree(p->right,q->right);
+        queue<TreeNode*>queue;
+        queue.push(p);
+        queue.push(q);
+        while(queue.size()!=0){
+            TreeNode*a=queue.front();
+            queue.pop();
+            TreeNode*b=queue.front();
+            queue.pop();
+            
+            if(a==NULL && b==NULL)
+                continue; // if both pair either left or right are null move to other pair either 
+                         // right or left resp.
+            if(a==NULL || b==NULL)
+                    return false;
+            
+            if(a->val != b->val)
+                return false;
+            
+            queue.push(a->left);
+            queue.push(b->left);
+            queue.push(a->right);
+            queue.push(b->right);
+        }
+        return true;
     }
 };
