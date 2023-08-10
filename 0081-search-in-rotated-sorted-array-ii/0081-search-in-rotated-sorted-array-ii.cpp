276c3661320f593e9ec0fb0f43eb82e3e7b24a37
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2023-08-10 23:44:30 +0530

    Time: 7 ms (53.92%), Space: 14 MB (18.44%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int i=0,j=nums.size()-1;
-        while(i<=j){
-            int mid=i+(j-i)/2;
+        int left=0,right=nums.size()-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
             if(nums[mid]==target)
                 return true;
-            //condition where this algo will not work 
-            //when low mid and high value are equal so shrink the arry
-            if(nums[i]==nums[mid] && nums[mid]==nums[j]){
-                i=i+1;
-                j=j-1;
-                continue; // bcz it may be possible that after shrinking again same value can encounter
+            if(nums[left]==nums[mid] && nums[mid]==nums[right]){
+                left=left+1;
+                right=right-1;
+                continue;
             }
             
-            if(nums[i]<=nums[mid]){
-                //left array sorted
-                if(target>=nums[i] && target<=nums[mid])
-                    j=mid-1;
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && nums[mid]>=target){
+                    right=mid-1;
+                }
                 else
-                    i=mid+1;
+                    left=mid+1;
             }
-           else{
-               //right array sorted
-               if(target>=nums[mid] && target<=nums[j])
-                   i=mid+1;
-               else
-                   j=mid-1;
-           }
+            else if(nums[mid]<=nums[right]){
+                if(target>=nums[mid] && target<=nums[right]){
+                    left=mid+1;
+                }
+                else
+                    right=mid-1;
+            }
         }
         return false;
     }
